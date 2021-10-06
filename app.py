02eff7c7448f21aaf5696d0d582e77dbc9e6561e
Gomezguti <gomezguti@gmail.com>
2021-10-06 00:19:01 -0500

    Ultimo

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-@app.route('0-1-1-1-Consulta datos Usuario Registrado')
-def index():
+@app.route('/0-1-1-1-Consulta datos Usuario Registrado')
+def Consulta_datos_Usuario_Registrado():
     return render_template('0-1-1-1-consulta_datos_usuario.html')
