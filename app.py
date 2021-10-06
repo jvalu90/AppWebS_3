8e3299ee53c003d47ee6c5eee924774ad4023702
j1arm1 <90942808+j1arm1@users.noreply.github.com>
2021-10-06 05:00:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,14 @@ app = Flask(__name__)
 @app.route('/0-1-1-1-consulta_datos_usuario')
 def consulta_datos_usuario():
     return render_template('0-1-1-1-consulta_datos_usuario.html')
+
+# Inicio Navegación usuario final registrado *************************************
+@app.route('/0-1-3-1-consulta_datos_usuario')
+def consulta_datos_usuario_final():
+    return render_template('0-1-3-1-consulta_datos_usuario.html')
+
+@app.route('/0-1-3-opciones_usuario_final_registrado')
+def opciones_usuario_final_registrado():
+    return render_template('0-1-3-opciones_usuario_final_registrado.html')
+
+# Fin Navegación usuario final registrado *************************************
