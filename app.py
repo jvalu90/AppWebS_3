67f0b7bdb24ea323a6736ebe484bdde3fe0c9347
jvalu90 <jvalu90@gmail.com>
2021-10-06 11:23:45 -0500

    Se actualiza el archivo app.py para incluir la ruta de inicio y primera ramifización de usuario final registrado

--- app.py
+++ app.py
@@ -2,9 +2,23 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+
+@app.route('/')
+def inicio():
+    return render_template('0-inicio.html')
+
+# Inicio de la aplicación ********************************************************
+
+@app.route('/usuario_registrado/', methods=['GET', 'POST'])
+def usuario_registrado():
+    return render_template('0-1-login.html')
+
+# Login **************************************************************************
+
 @app.route('/0-1-1-1-consulta_datos_usuario')
 def consulta_datos_usuario():
     return render_template('0-1-1-1-consulta_datos_usuario.html')
+
 
 # Inicio Navegación usuario final registrado *************************************
 @app.route('/0-1-3-1-consulta_datos_usuario')
