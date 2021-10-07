3dbf477e237897426eeb17ddf6ff88cdd819b435
jvalu90 <jvalu90@gmail.com>
2021-10-07 15:52:40 -0500

    Se inicia la construcción del forms

---
+++ forms.py
@@ -0,0 +1,6 @@
+#from flask_wtf import FlaskForm
+#from wtforms import validators
+#from wtforms.fields.core import StringField
+#from wtforms.fields.simple import SubmitField, TextAreaField
+
+#Iniciando la construcción del forms
