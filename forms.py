0420b97acdaf9a07cf80fe8cb4cfe11b8a41c0a1
jvalu90 <jvalu90@gmail.com>
2021-10-08 18:10:58 -0500

    Se depura error en 0-1-1

--- forms.py
+++ forms.py
@@ -9,5 +9,7 @@ class formlogin(FlaskForm):
     user = StringField('Usuario', validators=[validators.required(), validators.length(max=100)]) #Cómo se centra aquí?
     password = PasswordField('Contraseña', validators=[validators.required(), validators.length(max=100)])
     usuariofinal = SubmitField('Ingresar como Usuario Final') #<!--{{ form.usuariofinal(class="btn btn-outline-dark") }}-->
-    #administrador = SubmitField('Ingresar como Administrador')
+    administrador = SubmitField('Ingresar como Administrador')
+
+    # Choices, Choice, argumentos de RadioField
     #superadministrador = SubmitField('Ingresar como Super Administrador')
