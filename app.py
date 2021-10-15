2027b5fbc1ca6c1383e016a1313852ef9286cef1
jvalu90 <jvalu90@gmail.com>
2021-10-15 18:19:08 -0500

    Se actualiza modulo_reservas

--- app.py
+++ app.py
@@ -177,8 +177,7 @@ def modulo_reservas():
     else:  
         formulario = formreservas(request.form)
         if formulario.validate_on_submit():
-            objeto_reserva = reservas(0, 0, 0, 0, 0, formulario.initialdate.data, formulario.finaldate.data, 0, 0)
-            objeto_reserva.listado()
+            #Aquí es necesario llamar los campos del formulario.#nombre.data para enviar a los parámetros de listado
             return render_template('0-1-3-4-modulo_reservas.html', form=formulario, lista=reservas.listado(), mostrar = 1)
             #Pendiente desarrollar los métodos validadores aquí
         return render_template('0-1-3-4-modulo_reservas.html', mensaje="Todos los campos son obligatorios.", form=formulario)
